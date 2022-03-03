03cb2067cc74645afd13cf774ce20c879d6bcede
joscord <josecorderodev@gmail.com>
2022-03-02 22:55:32 -0300

    feat: modified index.js providing store to App using Provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Importamos el componente Provider de react-redux
+import { Provider } from 'react-redux';
+// Importamos nuestro store
+import store from './store';
 
+// En la prop store pasamos el valor de nuestra store. Proveemos así el store a nuestra app.
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
